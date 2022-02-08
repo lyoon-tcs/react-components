60433701817d85d3739e85f73e802feca01752bf
Lawrence Yoon <lyoon@truechoice.io>
2022-02-08 09:03:49 -0800

    feat: render Button, pass click me as text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // dependencies
 import React from 'react';
 // local files
+import Button from './components/Button/Button';
 
 const App = () => {
   return (
@@ -8,6 +9,9 @@ const App = () => {
       <a href="https://www.google.com" alt="Lawrence Yoon's profile">
         Lawrence Yoon
       </a>
+      <div className="buttons">
+        <Button buttonText="Click me!"></Button>
+      </div>
     </main>
   );
 };
