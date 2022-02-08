097e6855f963397b25df6543ca9a0322768dd7fd
Lawrence Yoon <lyoon@truechoice.io>
2022-02-08 09:28:12 -0800

    fix: add props validation for eslint fix

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,13 @@
 // dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = (props) => {
   return <button>{props.buttonText}</button>;
+};
+
+Button.propTypes = {
+  buttonText: PropTypes.string
 };
 
 export default Button;
