e610d16aee59e39707f66948708d6ec959aadf70
Lawrence Yoon <lyoon@truechoice.io>
2022-02-07 19:45:11 -0800

    test: update test description

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders Lawrence Link', () => {
+test('renders h1', () => {
   render(<App />);
   screen.debug();
   const linkElement = screen.getByText('Learning Jest + React');
