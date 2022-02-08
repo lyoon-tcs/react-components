b2f28c54e59bfefd416c9a4e9bbb05ce45159f1a
Lawrence Yoon <lyoon@truechoice.io>
2022-02-07 19:29:50 -0800

    feat: update App to only show h1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <main className="App">
-      <a href="https://www.google.com" alt="Lawrence Yoon's profile">
-        Lawrence Yoon
-      </a>
+      <h1>Learning Jest + React</h1>
     </main>
   );
 };
