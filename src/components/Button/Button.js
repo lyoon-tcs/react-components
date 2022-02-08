88c734b16e6836bf4f188a1fbef703324304f570
Lawrence Yoon <lyoon@truechoice.io>
2022-02-08 09:49:59 -0800

    feat: update

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,13 @@
 // dependencies
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 const Button = (props) => {
   return <button>{props.buttonText}</button>;
 };
 
-Button.propTypes = {
-  buttonText: PropTypes.string
-};
+// Button.propTypes = {
+//   buttonText: PropTypes.string
+// };
 
 export default Button;
