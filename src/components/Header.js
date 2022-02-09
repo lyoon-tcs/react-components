696608aec07c4b4b0e180127dd50cc235fa4b3e4
Lawrence Yoon <lyoon@truechoice.io>
2022-02-09 15:41:56 -0800

    doc: update Header.js to include copyright info

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-present TrueChoice IP Holding Company, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of TrueChoice IP Holding Company, Inc.
+ * ("Confidential Information").  You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered into with the company.
+ */
+
 // dependencies
 import React from 'react';
 import PropTypes from 'prop-types';
