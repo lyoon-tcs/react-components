f3ae359d62385a6501ed975ae40359cea807181b
Lawrence Yoon <lyoon@truechoice.io>
2022-02-09 15:36:20 -0800

    test: add Header test for proper text, add afterEach cleanup

--- src/components/tests/Header.test.js
+++ src/components/tests/Header.test.js
@@ -10,13 +10,25 @@
 // dependencies
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 // local files
 import Header from '../Header';
 
+afterEach(() => {
+  cleanup();
+});
+
 describe('Header Component renders correctly', () => {
+  const headerTitle = 'Hello world';
+
+  test('should render Header with proper text', () => {
+    render(<Header title={headerTitle} />);
+    const headingElement = screen.getByText(headerTitle);
+    expect(headingElement).toBeInTheDocument();
+  });
+
   test('matches snapshot', () => {
-    const tree = renderer.create(<Header title={'Hello world'} />).toJSON();
+    const tree = renderer.create(<Header title={headerTitle} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
