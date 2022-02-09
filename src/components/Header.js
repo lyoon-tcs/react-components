8c1c4d1dc06b67a57287c8418dbfb73bce990b5e
Lawrence Yoon <lyoon@truechoice.io>
2022-02-09 11:26:12 -0800

    fix: fix eslint propTypes error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 // dependencies
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = (props) => {
   return <h1 className="Header">{props.title}</h1>;
+};
+
+Header.propTypes = {
+  title: PropTypes.string
 };
 
 export default Header;
