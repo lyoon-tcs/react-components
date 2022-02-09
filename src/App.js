f3698f3de521b5f68c3cdf8c055715660ef820ca
Lawrence Yoon <lyoon@truechoice.io>
2022-02-09 13:47:22 -0800

    doc: get rid of questions and description, not needed for app

--- src/App.js
+++ src/App.js
@@ -12,17 +12,6 @@ import React from 'react';
 // local files
 import Button from './components/Button';
 
-/**
- * @description renders whole React application to the DOM.
- * @param {<type> e.g. number} a - (what to write for params?)
- * @param {<type> e.g. number} b - (what to write for params?)
- * @returns {<type> e.g. number} - JSX (what to write here?)
- * would I normally write tests for App, if it made use of other separate components? (No text)
- * how to let you know I've made necessary changes (will it show on same pull request), do I reply and resolve convo?
- * resolving conflicts
- * say I'm working on a branch and main gets updated - how to include all updates from main while working on branch?
- * if there's time - does your terminal show what branch you're on? Familiar with powerlevel10k - shows branch, tab auto
- */
 const App = () => {
   return (
     <main className="App">
