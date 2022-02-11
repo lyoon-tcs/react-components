24c714f28eb47458d685604c53f2723543c90bc2
Lawrence Yoon <lyoon@truechoice.io>
2022-02-11 09:29:48 -0800

    feat: add LearnStorybook js file

---
+++ src/LearnStorybook.js
@@ -0,0 +1,9 @@
+// dependencies
+import React, { Fragment } from 'react';
+// local files
+
+const LearnStorybook = () => {
+  return <Fragment>Learn Storybook!!!</Fragment>;
+};
+
+export default LearnStorybook;
