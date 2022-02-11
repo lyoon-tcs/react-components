f9dcd9b6752867e9c8c660cff5a828c649d4ef9a
Lawrence Yoon <lyoon@truechoice.io>
2022-02-11 09:18:14 -0800

    chore: add postcss and css modules as addons for storybook in main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,17 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  'stories': [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+  'addons': [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-postcss',
+    'storybook-css-modules-preset'
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
+  'framework': '@storybook/react',
+  'core': {
+    'builder': 'webpack5'
   }
-}
+};
