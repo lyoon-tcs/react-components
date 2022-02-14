32c79a34b4771f659de65ba36a955fdefd7411ef
Lawrence Yoon <lyoon@truechoice.io>
2022-02-14 09:01:12 -0800

    fix: change to lf

--- src/LearnStorybook.stories.js
+++ src/LearnStorybook.stories.js
@@ -11,23 +11,3 @@ export default {
 const Template = () => <LearnStorybook />;
 
 export const Default = Template.bind({});
-
-// // src/components/Button.stories.js
-// import React from 'react';
-
-// import Button from './Button';
-
-// export default {
-//   title: 'Example/Button',
-//   component: Button,
-//   argTypes: {
-//     backgroundColor: { control: 'color' }
-//   }
-// };
-
-// const Template = (args) => <Button {...args} />;
-
-// export const Default = Template.bind({});
-// Default.args = {
-//   label: 'My Button'
-// };
