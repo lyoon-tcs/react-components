9b88195b8a7262d281cc445f3f880ef40431b265
Lawrence Yoon <lyoon@truechoice.io>
2022-02-14 09:11:56 -0800

    refactor: create own Button.js for storybook

--- src/Button.js
+++ src/Button.js
@@ -6,3 +6,22 @@
  * ("Confidential Information").  You shall not disclose such Confidential Information and shall
  * use it only in accordance with the terms of the license agreement you entered into with the company.
  */
+
+// dependencies
+// local files
+// import PropTypes from 'prop-types';
+
+/**
+ * @description - returns Button component
+ * @param {string} props.buttonText - the button's text
+ * @return {jsx} - the Button component to render
+ */
+const Button = (props) => {
+  return <button>{props.buttonText}</button>;
+};
+
+// Button.propTypes = {
+//   buttonText: PropTypes.string
+// };
+
+export default Button;
