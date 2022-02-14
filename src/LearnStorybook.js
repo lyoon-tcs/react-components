f95c9ef18a189ac2bcb6f785ae91f91b797863f3
Lawrence Yoon <lyoon@truechoice.io>
2022-02-14 09:03:44 -0800

    refactor: delete LearnStorybook, start moving Button to storybook

--- src/LearnStorybook.js
+++
@@ -1,9 +0,0 @@
-// dependencies
-import React, { Fragment } from 'react';
-// local files
-
-const LearnStorybook = () => {
-  return <Fragment>Learn Storybook!!!</Fragment>;
-};
-
-export default LearnStorybook;
